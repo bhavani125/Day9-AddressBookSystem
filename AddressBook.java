1e1ee23e5c783a7786114221e6fb27f1637b25cc
bhavani125 <girinenibhavani12@gmil.com>
2021-07-12 06:25:22 +0530

    Added: Code for Editing Contacts

--- AddressBook.java
+++ AddressBook.java
@@ -6,11 +6,31 @@ import java.util.Scanner;
 public class AddressBook {
     //here iam using ArrayList to store
     public static ArrayList<Contacts> contactsArrayList = new ArrayList<>();
+    public static Scanner sc = new Scanner(System.in);
 
+    //main method
     public static void main(String[] args) {
-        System.out.println("Welcome to AddressBook Program");
+        boolean options = true;
+        while (options) {
+            System.out.println("Enter \n 1)To Add contacts \n 2) To edit contacts\n 3) To Exit");
+            System.out.println("Enter the option : ");
+            int option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    AddressBook.addContacts();
+                    break;
+                case 2:
+                    AddressBook.editContacts();
+                    break;
+                default:
+                    System.out.println("Invalid Option");
+            }
 
-        Scanner sc = new Scanner(System.in);
+        }
+    }
+
+    //creating a method for adding contacts
+    static void addContacts() {
         // creating object
         Contacts person = new Contacts();
 
@@ -29,15 +49,15 @@ public class AddressBook {
             person.setState(sc.next());
             System.out.println("Enter the EmailId");
             person.setEmailId(sc.next());
-            System.out.println("Enter the phoneNumber");
-            person.setPhoneNumber(sc.nextLong());
             System.out.println("Enter the zip");
             person.setZip(sc.nextLong());
+            System.out.println("Enter the phoneNumber");
+            person.setPhoneNumber(sc.nextLong());
 
-            //using console
+            //using toString
             System.out.println(person.toString());
             contactsArrayList.add(person);
-            System.out.println(contactsArrayList);
+
             System.out.println("Enter 1 add to another contact or Enter 0 to exit");
             int number = sc.nextInt();
             if (number == 0) {
@@ -45,6 +65,48 @@ public class AddressBook {
             }
         }
     }
-}
 
+    //creating method for editing contacts
+    static void editContacts() {
 
+        System.out.println("Enter firstname of the user you want to the edit:");
+        String firstName = sc.next();
+        for (Contacts c : contactsArrayList) {
+            if (c.getFirstName().equals(firstName)) {
+
+                System.out.println("c");
+                System.out.println("Enter the  field which u want to edit:");
+
+                System.out.println(" Address");
+                System.out.println(" City ");
+                System.out.println(" State");
+                System.out.println(" Email");
+                System.out.println(" Phone Number");
+                System.out.println(" ZipCode");
+                System.out.println("Exit");
+                String field = sc.next();
+
+                if (field.equals("firstName")) {
+                    c.setFirstName(sc.next());
+                } else if (field.equals("lastName")) {
+                    c.setLastName(sc.next());
+                } else if (field.equals("Address")) {
+                    c.setAddress(sc.next());
+                } else if (field.equals("City")) {
+                    c.setCity(sc.next());
+                } else if (field.equals("State")) {
+                    c.setState(sc.next());
+                } else if (field.equals("Email")) {
+                    c.setEmailId(sc.next());
+                } else if (field.equals("Zip")) {
+                    c.setZip(sc.nextLong());
+                } else if (field.equals("phoneNumber")) {
+                    c.setPhoneNumber(sc.nextLong());
+                }
+            }
+
+        }
+            System.out.println(contactsArrayList.toString());
+
+        }
+    }
